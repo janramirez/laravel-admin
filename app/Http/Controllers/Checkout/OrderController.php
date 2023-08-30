e0f338c0efd753b7dfb98a90e9109310d4629bad
janramirez <jramirez.ust@gmail.com>
2023-08-30 17:35:28 +0800

    Added checkout using stripe and mailer using mailcatcher

--- app/Http/Controllers/Checkout/OrderController.php
+++ app/Http/Controllers/Checkout/OrderController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Checkout;
 
 use App\Models\Link;
 use App\Models\Order;
-use App\Models\OrderItem;
 use App\Models\Product;
+use App\Models\OrderItem;
+use Cartalyst\Stripe\Stripe;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class OrderController
 {
@@ -32,7 +36,9 @@ class OrderController
 
         $order->save();
 
-        foreach($request->input('items') as $item) {
+        $lineItems = [];
+
+        foreach ($request->input('items') as $item) {
             $product = Product::find($item['product_id']);
 
             $orderItem = new OrderItem();
@@ -44,10 +50,63 @@ class OrderController
             $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
 
             $orderItem->save();
+
+            $lineItems[] = [
+                'name' => $product->title,
+                'description' => $product->description,
+                'images' => [
+                    $product->image,
+                ],
+                'amount' => 100 * $product->price,
+                'currency' => 'usd',
+                'quantity' => $orderItem->quantity,
+            ];
         }
+
+        try {
+            $stripe = Stripe::make(env('STRIPE_API_KEY'));
+        } catch (\Exception $e) {
+            Log::error("Error creating stripe instance: " . $e->getMessage());
+        };
+
+        $source = $stripe->checkout()->sessions()->create([
+            'payment_method_types' => ['card'],
+            'line_items' => $lineItems,
+            'success_url' => env('CHECKOUT_URL') . '/success?source={CHECKOUT_SESSION_ID}',
+            'cancel_url' => env('CHECKOUT_URL') . '/error'
+        ]);
+
+        $order->transaction_id = $source['id'];
+        $order->save();
 
         DB::commit();
 
-        return $order;
+        return $source;
+    }
+
+    public function confirm(Request $request)
+    {
+        if (!$order = Order::whereTransactionId($request->input('source'))->first()) {
+            return response([
+                'error' => 'Order not found!'
+            ], 404);
+        }
+
+        $order->complete = 1;
+        $order->save();
+
+        Mail::send('admin', ['order' => $order], function (Message $message) {
+            $message->to('admin@admin.com');
+            $message->subject('A new order has been completed!');
+        });
+
+        Mail::send('influencer', ['order' => $order], function (Message $message) use ($order) {
+            $message->to($order->influencer_email);
+            $message->subject('A new order has been completed!');
+        });
+
+        return response([
+            'message' => 'success',
+        ]);
     }
 }
