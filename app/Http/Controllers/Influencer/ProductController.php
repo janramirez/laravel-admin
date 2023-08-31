f5f09d2afb3b4f16908d210db9d35e2e5bcc33bb
janramirez <jramirez.ust@gmail.com>
2023-08-31 08:32:08 +0800

    Added caching using Redis

--- app/Http/Controllers/Influencer/ProductController.php
+++ app/Http/Controllers/Influencer/ProductController.php
@@ -5,18 +5,21 @@ namespace App\Http\Controllers\Influencer;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController
 {
     public function index(Request $request)
     {
-        $query = Product::query();
-
-        if($s = $request->input('s')) {
-            $query->whereRaw("title LIKE '%{$s}%'")
-            ->orwhereRaw("description LIKE '%{$s}%'");
-        }
-
-        return ProductResource::collection($query->get());
+        return Cache::remember('products', 60*30, function() use($request){
+            sleep(2);
+            $query = Product::query();
+    
+            if($s = $request->input('s')) {
+                $query->whereRaw("title LIKE '%{$s}%'")
+                ->orwhereRaw("description LIKE '%{$s}%'");
+            }
+            return ProductResource::collection($query->get());
+        });
     }
 }
